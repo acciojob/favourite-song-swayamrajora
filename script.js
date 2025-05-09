9b1a33eed9264aa2220cb688b935519b6ae6aa32
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-09 09:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
 //your code here
-<script>
-  document.getElementById('jaanetu').addEventListener('click', function(e) {
-    if (!this.href || this.href === '#') {
-      e.preventDefault();
-      alert('Invalid link!');
-    }
-  });
-</script>
+
